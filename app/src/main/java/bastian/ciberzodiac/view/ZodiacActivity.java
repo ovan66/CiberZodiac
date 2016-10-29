dfeddabfe598e4a1179a0ee0d7538717218efd5d
Bastian <bstian23@gmail.com>
2016-10-29 04:42:42 -0300

    eliminacion del fragmento Button

--- app/src/main/java/bastian/ciberzodiac/view/ZodiacActivity.java
+++ app/src/main/java/bastian/ciberzodiac/view/ZodiacActivity.java
@@ -1,6 +1,8 @@
 package bastian.ciberzodiac.view;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,11 +16,36 @@ import android.widget.RadioGroup;
 import bastian.ciberzodiac.R;
 import bastian.ciberzodiac.data.ResultZodiac;
 
+import static android.R.attr.name;
+
 public class ZodiacActivity extends AppCompatActivity {
+
+    private TextInputEditText nameEt;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zodiac);
+
+        nameEt = (TextInputEditText) findViewById(R.id.nameEt);
+        radioGroup = (RadioGroup) findViewById(R.id.dateRg);
+
+        Button button= (Button) findViewById(R.id.ResultBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEt.getText().toString();
+                int radioBtn = radioGroup.getCheckedRadioButtonId();
+                String date = "";
+                if (radioBtn != -1);
+                RadioButton radioButton = (RadioButton) radioGroup.findViewById(radioBtn);
+                date = radioButton.getText().toString();
+                ResultZodiac resultZodiac = new ResultZodiac(name,date,getBaseContext());
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getBaseContext());
+                dialog.setMessage(resultZodiac.result());
+                dialog.show();
+            }
+        });
     }
 }
